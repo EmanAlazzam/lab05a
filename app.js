66039e20e44e0b57ac3a8aab3d90986cc2821348
EmanAlazzam <emanalazzam577@gmail.com>
2021-03-06 21:25:49 +0200

    Added function

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ zz
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-// function sum(a, b) { //eslint-disable-line
-//     let sum=a+b;
-//     let arr1=[sum,' The sum of ' + a + ' and ' + b +' is ' + sum];
-//     return(arr1);
-//     }
+function sum(a, b) { //eslint-disable-line
+    let sum=a+b;
+    let arr1=[sum,' The sum of ' + a + ' and ' + b +' is ' + sum];
+    return(arr1);
+    }
     
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
@@ -28,11 +28,11 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // // Write your code here
-// function multiply(a, b) { //eslint-disable-line
-// let prdct=a*b;
-// let arr2=[prdct,' The multiplication of ' + a + ' and ' + b +' is ' + prdct];
-// return arr2;   
-// }
+function multiply(a, b) { //eslint-disable-line
+let prdct=a*b;
+let arr2=[prdct,' The multiplication of ' + a + ' and ' + b +' is ' + prdct];
+return arr2;   
+}
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
 
@@ -42,7 +42,7 @@ Test this function by hand in the console to get it working, and when you think 
 /* Problem 3
 Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
 
-Third element: "4 and 7 and 5 sum to 16."
+Third element:"4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
