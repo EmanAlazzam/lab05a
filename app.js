ab5d090edd1b1b4ccaa92d697e1491a33ed2d995
EmanAlazzam <emanalazzam577@gmail.com>
2021-03-06 20:35:21 +0200

    secon question has been added

--- app.js
+++ app.js
@@ -8,13 +8,12 @@ zz
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-    let sum=a+b;
-    let arr1=[sum,' The sum of ' + a + ' and ' + b +' is ' + sum];
-    return(arr1);
-    }
+// function sum(a, b) { //eslint-disable-line
+//     let sum=a+b;
+//     let arr1=[sum,' The sum of ' + a + ' and ' + b +' is ' + sum];
+//     return(arr1);
+//     }
     
-
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
 
@@ -30,9 +29,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+let prdct=a+b;
+let arr2=[prdct,' The multiplication of ' + a + ' and ' + b +' is ' + prdct];
+return(arr2);   
 }
-
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
 
